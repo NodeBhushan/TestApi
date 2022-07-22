ff43fd4cce1133f5eb50806e23a317161ccf33c5
Bhushan yellure <bhushan.yelu@gmail.com>
2022-07-22 15:08:29 +0530

    local to remote

---
+++ TestApi/Models/TestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApi.Models
+{
+    public class TestModel
+    {
+        public long UserId { get; set; }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
